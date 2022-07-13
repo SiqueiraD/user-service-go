f7e9b6c296a198954e465cdaf5cc2a84c1795ec9
danilo siqueira <danilosic.oliveira@gmail.com>
2022-07-13 16:04:28 -0300

    ago da

--- database/database.go
+++ database/database.go
@@ -13,7 +13,7 @@ import (
 var db *gorm.DB
 
 func StartDB() {
-	str := "sslmode=require host=ec2-34-233-115-14.compute-1.amazonaws.com port=5432 user=wwueqrowenxyjs dbname=djus4lp6a277j sslmode=disable password=d7fb29481b93549a5c5fc7d3384099f32e37c438b8dcee6e562f0a5b38fac9f6"
+	//str := "sslmode=require host=ec2-34-233-115-14.compute-1.amazonaws.com port=5432 user=wwueqrowenxyjs dbname=djus4lp6a277j sslmode=disable password=d7fb29481b93549a5c5fc7d3384099f32e37c438b8dcee6e562f0a5b38fac9f6"
 
 	database, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: "sslmode=require host=ec2-34-233-115-14.compute-1.amazonaws.com port=5432 user=wwueqrowenxyjs dbname=djus4lp6a277j sslmode=disable password=d7fb29481b93549a5c5fc7d3384099f32e37c438b8dcee6e562f0a5b38fac9f6", // data source name, refer https://github.com/jackc/pgx
