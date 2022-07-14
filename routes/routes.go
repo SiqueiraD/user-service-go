9c15b7b0d1f04c0ae91a200bdc9e7c54143b5210
danilo siqueira <danilosic.oliveira@gmail.com>
2022-07-13 23:17:56 -0300

    ajustando server golang

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,23 @@ import (
 	"github.com/meajudaaqui/user-service/controllers"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 	main := router.Group("user-api")
 	{
@@ -26,6 +43,6 @@ func ConfigRoutes(router *gin.Engine) *gin.Engine {
 			auth.POST("/", controllers.Autenticar)
 		}
 	}
-
+	router.Use(CORSMiddleware())
 	return router
 }
