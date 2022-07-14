520da4fab5d72765d72d37e39c15f195ddef3c2a
danilo siqueira <danilosic.oliveira@gmail.com>
2022-07-13 23:12:21 -0300

    ajustando cors

--- server/server.go
+++ server/server.go
@@ -15,14 +15,35 @@ type Server struct {
 
 func NewServer() Server {
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000" // Default port if not specified
+	}
 	return Server{
 		port:   port,
 		server: gin.Default(),
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func (s *Server) Run() {
 	router := routes.ConfigRoutes(s.server)
+	router.Use(CORSMiddleware())
 
 	log.Fatal(router.Run(":" + s.port))
 }
